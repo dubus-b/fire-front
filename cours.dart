e7a5f4dd872746f16cccea004360b78db2a0165e
Benjamin DUBUS <benjamin.dubus@outlook.com>
2023-03-01 00:46:15 +0100

    OOP

--- cours.dart
+++ cours.dart
@@ -1,14 +1,29 @@
 // en camelCase
 
-addNumbers(int n, int b) {
+class Person {
+  String name = "Benjamin";
+  int age = 25;
+}
+
+// dart est un language typé
+int addNumbers(int n, int b) {
   int result = n + b;
   return result;
 }
 
 void main() {
-  for (int i = 0; i < 5; i++) {
-    print('heloo ${i + 1}');
-  }
-  int test = addNumbers(1, 2);
-  print(test);
+  // for (int i = 0; i < 5; i++) {
+  //   print('heloo ${i + 1}');
+  // }
+  String name = "hello";
+
+  int result = addNumbers(1, 2);
+  var age = addNumbers(18, 1);
+  print(age);
+  print(result);
+  print(name);
+  var ben = Person();
+  print(ben.age);
+  var oscar = new Person();
+  oscar.name = "Oscar";
 }
